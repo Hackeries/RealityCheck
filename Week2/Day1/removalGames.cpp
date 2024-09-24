3f97f2755488816f9e12bff93daa518773f7619e
Hackeries <itssaj15@gmail.com>
2024-09-24 19:41:14 +0530

    Week2Day1

---
+++ Week2/Day1/removalGames.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define lli long long
+#define pii pair<lli,lli>
+#define fi first
+#define sc second
+#define pb push_back
+#define EB emplace_back
+#define MP make_pair
+#define vi vector<lli>
+#define vov vector<vector<lli>>v(n,vector<lli> (m))
+#define vpii vector<pair<lli,lli>>
+#define all(o) (o).begin(), (o).end()
+#define fr(i,n) for(lli i=0;i<n;++i)
+#define br(n,i) for(lli i=n;i>=0;--i)
+#define vovfr(i,n,m) fr(i,n){fr(j,m){}}
+#define rep(i,a,b) for(lli i=a;i<=b;++i)
+#define per(i,b,a) for(lli i=b;i>=a;--i)
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+const lli MOD = 1e9 + 7;
+lli gcd(lli a, lli b){return b == 0 ? a : gcd(b,a%b);}
+lli binpow(lli base, lli expo){
+    if(expo == 0)return 1;
+
+    if(expo%2 == 1){
+        return (base * binpow(base , expo-1)) % MOD;
+    }else{
+        lli temp = binpow(base , expo / 2);
+        return (temp * temp) % MOD;
+    }
+}
+
+void solve(){
+    lli n;cin >> n;
+    vi v(n),v2(n);
+    fr(i,n)cin >> v[i];
+    fr(i,n)cin >> v2[i];
+    
+    // lli x = v.size()/2;
+    // lli y = v2.size()/2;
+    if(n == 2 || n == 1){
+        cout << "Bob\n";
+        return;
+    }
+    // cout << v[x] << " " << v2[y] << "\n";
+    // if(v[x] == v2[y])cout << "Bob\n";
+    // else cout << "Alice\n";
+    vi ans = v;
+    reverse(all(ans));
+    bool ok = (v==v2);
+    bool okk = (ans == v2);
+    
+    if(ok || okk)cout << "Bob\n";
+    else cout << "Alice\n";
+}
+
+signed main(){
+    fastIO;
+    lli t=1;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+}
