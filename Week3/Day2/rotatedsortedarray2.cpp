642bbc46ba8ba235fe63736e7671d057dc869aef
Hackeries <itssaj15@gmail.com>
2024-10-17 00:16:42 +0530

    week3day2

---
+++ Week3/Day2/rotatedsortedarray2.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define lli long long
+#define pii pair<lli,lli>
+#define fi first
+#define sc second
+#define pb push_back
+#define EB emplace_back
+#define MP make_pair
+#define vi vector<lli>
+#define vov vector<vector<lli>>v(n,vector<lli> (m))
+#define vpii vector<pair<lli,lli>>
+#define all(o) (o).begin(), (o).end()
+#define fr(i,n) for(lli i=0;i<n;++i)
+#define br(n,i) for(lli i=n;i>=0;--i)
+#define vovfr(i,n,m) fr(i,n){fr(j,m){}}
+#define rep(i,a,b) for(lli i=a;i<=b;++i)
+#define per(i,b,a) for(lli i=b;i>=a;--i)
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+const lli MOD = 1e9 + 7;
+lli gcd(lli a, lli b){return b == 0 ? a : gcd(b,a%b);}
+lli binpow(lli base, lli expo){
+    if(expo == 0)return 1;
+
+    if(expo%2 == 1){
+        return (base * binpow(base , expo-1)) % MOD;
+    }else{
+        lli temp = binpow(base , expo / 2);
+        return (temp * temp) % MOD;
+    }
+}
+
+lli solve(){
+    lli n,x;cin >> n;
+    vi v(n);fr(i,n)cin >> v[i];
+    cin >> x;
+    
+    lli s = 0,e = n-1;
+    while(s<=e){
+        lli mid = s + (e-s)/2;
+        
+        if(v[mid] == x)return mid;
+        if(v[s] == v[mid] && v[mid] == v[e]){
+            s++;e--;continue;
+        }
+        
+        if(v[s] <= v[mid]){
+            if(v[mid] >= x && v[s] <= x)e = mid - 1;
+            else s = mid + 1;
+        }else{
+            if(v[mid]<=x && x <= v[e])s = mid + 1;
+            else e = mid - 1;
+        }
+    }
+    return -1;
+}
+
+signed main(){
+    fastIO;
+    lli t=1;
+    // cin >> t;
+
+    while(t--){
+        cout << solve() << "\n";
+    }
+}
